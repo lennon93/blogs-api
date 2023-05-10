36cf75b2ae239ab4510d58eebd60b12fd34619aa
Lennon <lennon_lxs@hotmail.com>
2023-05-10 18:42:34 -0300

    Inicia o projeto

--- src/app.js
+++ src/app.js
@@ -11,8 +11,6 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 
-// ...
-
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
