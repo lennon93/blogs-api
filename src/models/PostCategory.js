338eb8675168ce66648b79e76bc09aa2e5592e51
Lennon <lennon_lxs@hotmail.com>
2023-05-15 02:46:34 -0300

    cria o model postCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      categoryId: { type: DataTypes.INTEGER }, 
+      postId: { type: DataTypes.INTEGER }  
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'posts_categories',
+    },
+  );
+  
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+    return PostCategory;
+  };
